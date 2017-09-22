6641a3731b1f429973997293b43ab605d31c77b8
Panos Ipeirotis <ipeirotis@gmail.com>
2017-09-22 16:58:46 +0000

    New sync mechanism

--- sync_notebooks.sh
+++ sync_notebooks.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo "############################################################"
-echo "This will delete the current content of NYU_Notes"
-echo "and replace it with the content from the Github repository"
+echo "This will replace the content of NYU_Notes"
+echo "the most recent content from the Github repository"
 echo ""
-echo "Please ensure that you have saved your own notebooks"
-echo "into the Student_Notebooks folder"
+echo "The existing NYU_Notes folder will be renamed"
+echo "NYU_Notes_"$(date '+%Y-%b-%d_%H%M')
+echo "and preserved until you delete it. "
 echo "############################################################"
 echo ""
 read -p "Are you sure that you want to proceed? (Y/N) " -n 1 -r
-echo    # (optional) move to a new line
+echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   cd /home/ubuntu/jupyter
-  rm -rf NYU_Notes
+  mv NYU_Notes NYU_Notes_$(date '+%Y-%b-%d_%H%M')
   git clone https://github.com/ipeirotis/dealing_with_data.git NYU_Notes
   cd
 fi
 
+rm /home/ubuntu/sync_notebooks.sh 
+ln -s /home/ubuntu/jupyter/NYU_Notes/sync_notebooks.sh /home/ubuntu/sync_notebooks.sh 
