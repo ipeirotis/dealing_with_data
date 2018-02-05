f569fece6ace6d88bbc3f2cfe23417fc40b7ca25
Panos Ipeirotis <ipeirotis@gmail.com>
2018-02-05 21:12:17 +0000

    Small changes to the notes

--- sync_data.sh
+++ sync_data.sh
@@ -3,3 +3,6 @@
 sudo rm -rf /data
 sudo mkdir -p /data
 sudo git clone https://github.com/ipeirotis/data.git /data
+
+rm /home/ubuntu/sync_data.sh 
+ln -s /home/ubuntu/jupyter/NYU_Notes/sync_data.sh /home/ubuntu/sync_data.sh 
